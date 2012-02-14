03fa4d3bf265a56d55971584497ae5a093ab9eca
gailo22 <gailo22@gmail.com>
2012-02-14 21:25:24 +0800

    changed project structure to be maven

---
+++ src/main/java/adapter/Shape.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adapter;
+
+/**
+ *
+ * @author User
+ */
+public interface Shape {
+    void draw(int x1, int y1, int x2, int y2);
+}
